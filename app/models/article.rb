3f0151beb3a6f5852ef2e4043491da8df837a6ce
Celso de Sa <celsodessa@gmail.com>
2014-08-10 17:20:40 -0300

    add dedeplication method to Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -16,17 +16,17 @@ class Article < ActiveRecord::Base
 	).valid?
   end
 
-  #def self.deduplicate #procurava por duplicatas e as destruir
-    # find all models and group them on keys which should be common
-  #  grouped = all.group_by{|model| [model.title, model.article_url, model.site_id] }
-  #  grouped.values.each do |duplicates|
+  def self.deduplicate #procurava por duplicatas e as destruir
+     #find all models and group them on keys which should be common
+    grouped = all.group_by{|model| [model.title, model.article_url, model.site_id] }
+    grouped.values.each do |duplicates|
       # the first one we want to keep right?
-  #    first_one = duplicates.shift # or pop for last one
+      first_one = duplicates.shift # or pop for last one
       # if there are any more left, they are duplicates
       # so delete all of them
-  #   duplicates.each{|double| double.destroy} # duplicates can now be destroyed
-  #  end
-  #end
+     duplicates.each{|double| double.destroy} # duplicates can now be destroyed
+    end
+  end
 
   #def self.keywords_dedup
   #	articles = Article.all
