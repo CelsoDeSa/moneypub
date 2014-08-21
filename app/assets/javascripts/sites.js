5d7366086797e34024158165096a081e64056174
Celso de Sa <celsodessa@gmail.com>
2014-08-21 20:39:40 -0300

    dont know what to do with this gif problem

--- app/assets/javascripts/sites.js
+++ app/assets/javascripts/sites.js
@@ -1,11 +1,12 @@
-$(document).ready(function() {
-  $('#loading-indicator').hide();  // hide it initially.
-  $(document)  
-    .ajaxStart(function() {
-      $('#search').remove();
-      $('#loading-indicator').show(); // show on any Ajax event.
-    })
-    .ajaxStop(function() {
-      $('#loading-indicator').hide(); // hide it when it is done.
-  });
-});
+$(document).ready(
+	function() { // hide it initially.
+	  $(document)  
+	    .ajaxStart(function() {
+	      $('#search').remove();
+	      $('#loading-indicator').show(); // show on any Ajax event.
+	    })
+	    .ajaxStop(function() {
+	      $('#loading-indicator').hide(); // hide it when it is done.
+  		});
+	}
+);
