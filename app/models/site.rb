be4cb315322cd7c6d0aeeb3c6905a65606b12c3c
Celso de Sa <celsodessa@gmail.com>
2014-08-21 22:55:17 -0300

    correction on Site.alexa_calc()

--- app/models/site.rb
+++ app/models/site.rb
@@ -11,23 +11,23 @@ class Site < ActiveRecord::Base
 	#end
 
 	def self.alexa_calc(n)
-		if n >= 91000
+		if n >= 90001
 			@values[0] = 1
-		elsif (81000..90000) === n
+		elsif (80001..90000) === n
 			@values[0] = 2
-		elsif (71000..80000) === n
+		elsif (70001..80000) === n
 			@values[0] = 3
-		elsif (61000..70000) === n
+		elsif (60001..70000) === n
 			@values[0] = 4
-		elsif (51000..60000) === n
+		elsif (50001..60000) === n
 			@values[0] = 5
-		elsif (41000..50000) === n
+		elsif (40001..50000) === n
 			@values[0] = 6
-		elsif (31000..40000) === n
+		elsif (30001..40000) === n
 			@values[0] = 7
-		elsif (21000..30000) === n
+		elsif (20001..30000) === n
 			@values[0] = 8
-		elsif (11000..20000) === n
+		elsif (10001..20000) === n
 			@values[0] = 9
 		elsif (1..10000) === n
 			@values[0] = 10
