3aebdb8bb6ae8daca75d41a4fd3a77e6a4fade06
Celso de Sa <celsodessa@gmail.com>
2014-08-21 21:14:41 -0300

    deixei tudo como estava, vamos ver no que da com esse loading gif

--- app/assets/javascripts/sites.js
+++ app/assets/javascripts/sites.js
@@ -1,12 +1,11 @@
-$(document).ready(
-	function() { // hide it initially.
-	  $(document)  
-	    .ajaxStart(function() {
-	      $('#search').remove();
-	      $('#loading-indicator').show(); // show on any Ajax event.
-	    })
-	    .ajaxStop(function() {
-	      $('#loading-indicator').hide(); // hide it when it is done.
-  		});
-	}
-);
+$(function() {
+  $('#loading-indicator').hide();  // hide it initially.
+  $(document)  
+    .ajaxStart(function() {
+      $('#search').remove();
+      $('#loading-indicator').show(); // show on any Ajax event.
+    })
+    .ajaxStop(function() {
+      $('#loading-indicator').hide(); // hide it when it is done.
+  });
+});
