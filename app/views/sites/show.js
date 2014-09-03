0828ebef2e6ddfd9d60abd60da0c2567c7358129
Celso de Sa <celsodessa@gmail.com>
2014-09-02 21:19:15 -0300

    updating master

--- app/views/sites/show.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-  if ($('#about').length > 0) { 
-    $('#about').remove();
-    $('.confidenceindex').append('<%= j render "about" %>');  
-  } else {
-    $('.confidenceindex').append('<%= j render "about" %>');
-  }
-});
